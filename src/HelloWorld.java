63760dad7f57e637973a603a24e97387cf899448
Luis Montealegre <montealegreluis@gmail.com>
2017-09-18 11:34:18 -0500

    Create "Hello, world!" in Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+
+public class HelloWorld {
+    // psvm
+    // Entry point
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello world!");
+        System.out.println("Hello Redwood!");
+    }
+}
