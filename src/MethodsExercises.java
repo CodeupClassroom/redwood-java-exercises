b5b90126c3af28d5f3bed8f656fca0cd6aa14910
Fernando <fernandoza@gmail.com>
2017-09-22 16:27:21 -0500

    Finished the last 2 exercises.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
+
     public static void main(String[] args) {
 
-        factorial(getInteger(1, 10));
+        Scanner scan = new Scanner(System.in);
+
+        game101(scan);
+//
+//        System.out.print("Provide the number of sides; ");
+//        rollDice(getInteger(1,10, scan), scan);
+
+//        factorial(getInteger(1, 10, scan));
 
 //        getInteger(1, 10);
 
@@ -34,15 +42,14 @@ public class MethodsExercises {
         return n / m;
     }
 
-    public static int getInteger(int min, int max){
+    public static int getInteger(int min, int max, Scanner scan){
 
-        Scanner scan = new Scanner(System.in);
         System.out.println("Give me an Integer between " + min + " and  " + max);
         int input = Integer.parseInt(scan.nextLine());
 
         if(input < min || input > max){
             System.out.println("Outside of the limit");
-            getInteger(min, max);
+            getInteger(min, max, scan);
         }else{
             System.out.println("Valid input");
             return input;
@@ -58,6 +65,50 @@ public class MethodsExercises {
             details += (i == 1) ? i : " x " + i;
             System.out.println( i +"! = " + details + " = " + ( acum *= i ) );
         }
+
+    }
+
+    public static void rollDice(int sides, Scanner input){
+
+        while(true){
+            System.out.println("Type roll to start");
+            String usersInput = input.nextLine();
+
+            if(usersInput.trim().equalsIgnoreCase("roll")){
+                System.out.println("dices: " + generateRandom(1, sides) + " " + generateRandom(1, sides));
+            }else{
+                System.out.println("Not a valid option, exiting...");
+                break;
+            }
+        }
+
+    }
+
+    public static int generateRandom(int min, int max){
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
+
+    public static void game101(Scanner scan){
+
+        int random = generateRandom(1, 100);
+        int number = 0;
+        System.out.println("random" + random);
+
+        while(true){
+
+            number = getInteger(1, 100, scan);
+
+            if(number < random){
+                System.out.println("Higher");
+            }else if(number > random){
+                System.out.println("Lower");
+            }else if(number == random){
+                System.out.println("You guessed right!");
+                break;
+            }
+
+        }
+
 
     }
 
