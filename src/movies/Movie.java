28af2e29a81100949bd7f754e208cdf84bca55bc
Fernando <fernandoza@gmail.com>
2017-09-27 15:25:39 -0500

    Creates the Movie class.
    - Adds the properties.
    - Creates the constructor.

---
+++ src/movies/Movie.java
@@ -0,0 +1,20 @@
+package movies;
+
+public class Movie {
+
+    private String name;
+    private String category;
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
