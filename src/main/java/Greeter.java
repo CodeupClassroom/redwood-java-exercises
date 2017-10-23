6786ac2f2fa7d176f69aae47dfe535328f947f5a
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 15:18:31 -0500

    Create an interface called Greeter

---
+++ src/main/java/Greeter.java
@@ -0,0 +1,8 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+package main.java;
+
+/* Create an interface called Greeter */
+public interface Greeter {
+}
