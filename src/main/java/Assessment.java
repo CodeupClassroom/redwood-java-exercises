abd2d5a26256737054f7de909c51e2a0b7883e79
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 13:50:23 -0500

    Create static method square and add some tests for it

--- src/main/java/Assessment.java
+++ src/main/java/Assessment.java
@@ -4,4 +4,16 @@
 package main.java;
 
 public class Assessment {
+    /*
+     * Write a static method called `square(number)` accepts an integer as input
+     * and returns that number times itself.
+     */
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The square of 3 is " + square(3));
+        System.out.println("The square of -2 is " + square(-2));
+    }
 }
