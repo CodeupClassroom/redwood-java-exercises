0f6f0443695e99d08554fee822d8330962d57d22
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 14:29:13 -0500

    If the firstName or lastName inputs are null,  throw an IllegalArgumentException inside of the constructor.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -8,4 +8,18 @@ public class Person {
     /* Add firstName and lastName as protected properties. */
     protected String firstName;
     protected String lastName;
+
+    /* Add a constructor method that takes in two strings that are firstName and lastName. */
+    public Person(String firstName, String lastName) {
+        /*
+         * If the firstName or lastName inputs are null, be sure to throw an IllegalArgumentException
+         * inside of the constructor.
+         */
+        if (firstName == null || lastName == null) {
+            throw new IllegalArgumentException("Neither the first nor the last name can be null");
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
