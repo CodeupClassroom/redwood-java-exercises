471921540458ef948d98677eb5b6b3d4eb5b7c8f
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 13:54:55 -0500

    Create static method sum
    * Create overload for integers
    * Create overload for floats
    * Add tests for both

--- src/main/java/Assessment.java
+++ src/main/java/Assessment.java
@@ -12,8 +12,22 @@ public class Assessment {
         return number * number;
     }
 
+    public static int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static float sum(float number1, float number2) {
+        return number1 + number2;
+    }
+
     public static void main(String[] args) {
         System.out.println("The square of 3 is " + square(3));
         System.out.println("The square of -2 is " + square(-2));
+        System.out.println("Tests for sum using integers");
+        System.out.println("4 + (-18) is " + sum(4, -18));
+        System.out.println("(-4) + (-4) is " + sum(-4, -4));
+        System.out.println("Tests for sum using floats");
+        System.out.println("4.5 + (-15.5) is " + sum(4.5f, -15.5f));
+        System.out.println("(-4.4) + (-4.6) is " + sum(-4.4f, -4.6f));
     }
 }
