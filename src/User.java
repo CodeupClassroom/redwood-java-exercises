bab2968af37a39a0736d9c0c16faa6adb73d8605
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 15:12:52 -0500

    Add a protected property called isAdmin that is a boolean

--- src/User.java
+++ src/User.java
@@ -6,6 +6,9 @@ import main.java.Person;
 
 /* Create a class called User that inherits from Person */
 public class User extends Person {
+    /* Add a protected property called isAdmin that is a boolean */
+    protected boolean isAdmin;
+
     public User(String firstName, String lastName) {
         super(firstName, lastName);
     }
