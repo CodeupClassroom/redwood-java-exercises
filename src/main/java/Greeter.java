88e32a2b7d5fd5abdb40316c5626b5abd670fd39
Luis Montealegre <montealegreluis@gmail.com>
2017-10-23 15:19:08 -0500

    Greeter interface should specify a method called sayHello()

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -5,4 +5,6 @@ package main.java;
 
 /* Create an interface called Greeter */
 public interface Greeter {
+    /* Greeter interface should specify a method called sayHello() */
+    String sayHello();
 }
